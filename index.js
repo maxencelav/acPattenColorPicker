e6d0173436feaa41084669a11fc72b016f226e4e
Maxence Lavenu <50020222+maxencelav@users.noreply.github.com>
2020-03-28 17:50:30 +0100

    WIP Game Conversion

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
-var colorPicker = new iro.ColorPicker('#picker');
+var colorPicker = new iro.ColorPicker('#picker', {
+  layout: [{
+      component: iro.ui.Wheel,
+      options: {
+        borderWidth: 3
 
-colorPicker.on('color:change', function(color) {
-    updateColors();
+      }
+    },
+    {
+      component: iro.ui.Slider,
+      options: {
+        sliderType: 'hue', // can also be 'saturation', 'value', 'alpha' or 'kelvin'
+        borderWidth: 3
+      }
+    },
+    {
+      component: iro.ui.Slider,
+      options: {
+        sliderType: 'saturation', // can also be 'saturation', 'value', 'alpha' or 'kelvin'
+        borderWidth: 3
+      }
+    },
+    {
+      component: iro.ui.Slider,
+      options: {
+        sliderType: 'value', // can also be 'saturation', 'value', 'alpha' or 'kelvin'
+        borderWidth: 3
+      }
+    }
+  ]
 });
 
-function updateColors(){
-    document.getElementById("hue").innerHTML = colorPicker.color.hsl["h"];
-    document.getElementById("viv").innerHTML = colorPicker.color.hsl["s"];
-    document.getElementById("bri").innerHTML = colorPicker.color.hsl["l"];
+
+colorPicker.on('color:change', function (color) {
+  console.log(colorPicker.color.hsl);
+  updateColorsLive();
+  updateColorsIngame();
+
+});
+
+function updateColorsLive() {
+  document.getElementById("hue-live").innerHTML = colorPicker.color.hsl["h"];
+  document.getElementById("viv-live").innerHTML = colorPicker.color.hsl["s"];
+  document.getElementById("bri-live").innerHTML = colorPicker.color.hsl["l"];
+
+}
+
+function updateColorsIngame() {
+  document.getElementById("hue-ing").innerHTML = Math.floor(colorPicker.color.hsl["h"]/ 30);
+  document.getElementById("viv-ing").innerHTML = Math.floor(colorPicker.color.hsl["s"]/15);
+  document.getElementById("bri-ing").innerHTML = Math.floor(colorPicker.color.hsl["l"]/15);
 
 }
