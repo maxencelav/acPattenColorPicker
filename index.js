39a1cbde1a9242c6e4e93bdaf60681daae25c288
Maxence Lavenu <50020222+maxencelav@users.noreply.github.com>
2020-03-29 00:43:44 +0100

    Live Ingame Preview

--- index.js
+++ index.js
@@ -69,6 +69,11 @@ function updateColorsLive() {
   document.getElementById("rgb-color").value = colorPicker.color.hexString;
   document.getElementById("color-viewer").style.backgroundColor = document.getElementById("rgb-color").value;
 
+  document.getElementById("img-viv").style.filter = "hue-rotate(" + colorPicker.color.hue + "deg)";
+  document.getElementById("img-bri").style.filter = "hue-rotate(" + colorPicker.color.hue + "deg)";
+
+
+
 
 
 
@@ -77,8 +82,158 @@ function updateColorsLive() {
 }
 
 function updateColorsIngame() {
-  document.getElementById("hue-ing").innerHTML = Math.floor((colorPicker.color.hsl["h"] * 30) / 360);
-  document.getElementById("viv-ing").innerHTML = Math.floor((colorPicker.color.hsl["s"] * 15) / 100);
-  document.getElementById("bri-ing").innerHTML = Math.floor((colorPicker.color.hsl["l"] * 15) / 100);
+
+  let hue = Math.floor((colorPicker.color.hsl["h"] * 29) / 360);
+  let sat = Math.floor((colorPicker.color.hsl["s"] * 14) / 100);
+  let lig = Math.floor((colorPicker.color.hsl["l"] * 14) / 100);
+
+  let leftHue;
+  let leftSat;
+  let leftLig;
+
+  document.getElementById("hue-ing").innerHTML = hue + 1;
+  document.getElementById("viv-ing").innerHTML = sat + 1;
+  document.getElementById("bri-ing").innerHTML = lig + 1;
+
+  leftHue = hue * 0.665 - 0.6;
+
+  switch (sat) {
+    case 0:
+      leftSat = -0.3;
+      break;
+
+    case 1:
+      leftSat = 1.1;
+      break;
+    case 2:
+      leftSat = 2.4;
+      break;
+
+    case 3:
+      leftSat = 3.75;
+      break;
+
+    case 4:
+      leftSat = 5.1;
+      break;
+
+    case 5:
+      leftSat = 6.5;
+      break;
+
+    case 6:
+      leftSat = 7.8;
+      break;
+
+    case 7:
+      leftSat = 9.2;
+      break;
+
+    case 8:
+      leftSat = 10.45;
+      break;
+
+    case 9:
+      leftSat = 11.8;
+      break;
+
+    case 10:
+      leftSat = 13.2;
+      break;
+
+    case 11:
+      leftSat = 14.65;
+      break;
+
+    case 12:
+      leftSat = 16;
+      break;
+
+    case 13:
+      leftSat = 17.17;
+      break;
+
+
+    case 14:
+      leftSat = 18.5;
+      break;
+
+    default:
+      leftSat = sat * 1.22 + 0.09;
+      break;
+  }
+
+  switch (lig) {
+    case 0:
+      leftLig = -0.3;
+      break;
+
+    case 1:
+      leftLig = 0.9;
+      break;
+    case 2:
+      leftLig = 2.1;
+      break;
+
+    case 3:
+      leftLig = 3.5;
+      break;
+
+    case 4:
+      leftLig = 4.8;
+      break;
+
+    case 5:
+      leftLig = 6.2;
+      break;
+
+    case 6:
+      leftLig = 7.6;
+      break;
+
+    case 7:
+      leftLig = 8.9;
+      break;
+
+    case 8:
+      leftLig = 10.25;
+      break;
+
+    case 9:
+      leftLig = 11.6;
+      break;
+
+    case 10:
+      leftLig = 12.9;
+      break;
+
+    case 11:
+      leftLig = 14.3;
+      break;
+
+    case 12:
+      leftLig = 15.6;
+      break;
+
+    case 13:
+      leftLig = 17;
+      break;
+
+    case 14:
+      leftLig = 18.3;
+      break;
+
+    default:
+      leftLig = lig * 1.22 + 0.09;
+      break;
+  }
+
+  //leftLig = lig * 1.22 + 0.09;
+
+
+  document.getElementById("hue-preview").firstElementChild.style.left = leftHue + "em";
+  document.getElementById("viv-preview").firstElementChild.style.left = leftSat + "em";
+  document.getElementById("bri-preview").firstElementChild.style.left = leftLig + "em";
+
 
 }
